26ec0e871cc82c814ecc4465279131aefdfd76a1
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-14 11:44:41 +0900

    #2 [Feat]: Async 확장 함수 추가
    - (Flow to Async) 확장함수
    - Async (onSuccess, onError) 확장함수

--- domain/src/main/kotlin/com/citysavior/android/domain/model/common/Async.kt
+++ domain/src/main/kotlin/com/citysavior/android/domain/model/common/Async.kt
@@ -1,5 +1,10 @@
 package com.citysavior.android.domain.model.common
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
 /**
  * A generic class that holds a value with its [Loading] and [Error] status.
  *
@@ -11,4 +16,29 @@ sealed class Async<out T> {
     data class Error(val exception: Throwable) : Async<Nothing>()
 
     data class Success<out T>(val data: T) : Async<T>()
+}
+
+/**
+ * Convert [Flow] to [Async] type.
+ */
+fun <T> Flow<T>.toAsync(): Flow<Async<T>> = map<T, Async<T>> { Async.Success(it) }
+    .onStart { emit(Async.Loading) }
+    .catch { e -> emit(Async.Error(e)) }
+
+/**
+ * Performs the given [action] on the encapsulated value if this instance represents [Async.Success].
+ * Returns the original `Async` unchanged.
+ */
+inline fun <T> Async<T>.onSuccess(action: (T) -> Unit): Async<T> {
+    if (this is Async.Success) action(data)
+    return this
+}
+
+/**
+ * Performs the given [action] on the encapsulated value if this instance represents [Async.Error].
+ * Returns the original `Async` unchanged.
+ */
+inline fun <T> Async<T>.onError(action: (Throwable) -> Unit): Async<T> {
+    if (this is Async.Error) action(exception)
+    return this
 }
