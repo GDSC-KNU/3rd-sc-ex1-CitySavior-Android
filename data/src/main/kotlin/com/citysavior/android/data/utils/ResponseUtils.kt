dd5668776b76a51e53f68a98313217da234586a6
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-14 12:36:55 +0900

    #2 [Feat]: api 호출 일반화 함수 추가
    - api 호출이후 Async로 변환하는 함수 추가
    - 특정 시간마다 api호출하여 flow로 변환하는 함수 추가

---
+++ data/src/main/kotlin/com/citysavior/android/data/utils/ResponseUtils.kt
@@ -0,0 +1,61 @@
+package com.citysavior.android.data.utils
+
+import com.citysavior.android.domain.model.common.Async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+
+/**
+ * Convert [Response] to [Async] type.
+ *
+ * Return value is [Async.Success] if response is successful and body is not null.
+ *
+ * Return value is [Async.Error] if response is not successful or body is null or convert error.
+ *
+ * Return Async is Safe to use.
+ */
+suspend fun <Data, Domain> invokeApiAndConvertAsync(
+    api: suspend () -> Response<Data>,
+    convert: Data.() -> Domain
+): Async<Domain> {
+    return try {
+        val response = api.invoke()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            Async.Success((body as Data?)!!.convert())
+        } else {
+            Async.Error(Throwable(response.message()))
+        }
+    } catch (e: Exception) {//response body convert error
+        Async.Error(e)
+    }
+}
+
+/**
+ * Convert Api response to [Flow] type.
+ *
+ * Return value is [Async.Success] if response is successful and body is not null.
+ *
+ * Throw [Throwable] if response is not successful or body is null or convert error.
+ *
+ * Repeat invoke api with [intervalMillis] interval.
+ *
+ * Error caught when [Flow.toAsync] is called.
+ */
+suspend fun <Data, Domain> invokeApiAndMakeFlow(
+    api: suspend () -> Response<Data>,
+    intervalMillis: Long,
+    convert: Data.() -> Domain
+): Flow<Domain> = flow {
+    while (true) {
+        val response = api.invoke()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            emit((body as Data?)!!.convert())
+        } else {
+            throw Throwable(response.message())
+        }
+        delay(intervalMillis)
+    }
+}
