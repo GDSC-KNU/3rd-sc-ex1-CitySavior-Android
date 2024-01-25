09ab5f374dac7970549a7a1376805eb28b744fe9
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-25 23:57:03 +0900

    #5 [Feat]: 메인화면 viewmodel 추가

---
+++ app/src/main/kotlin/com/citysavior/android/MainActivityViewModel.kt
@@ -0,0 +1,41 @@
+package com.citysavior.android
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.citysavior.android.domain.model.common.onError
+import com.citysavior.android.domain.model.common.onSuccess
+import com.citysavior.android.domain.repository.auth.AuthRepository
+import com.citysavior.android.domain.repository.auth.JwtTokenRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    private val jwtTokenRepository: JwtTokenRepository,
+    private val authRepository: AuthRepository,
+) : ViewModel() {
+    val uiState: StateFlow<MainActivityUiState> = flow {
+        authRepository.getAfterOnBoarding()
+            .onSuccess {
+                if (it.not()) {
+                    emit(MainActivityUiState.Initial)
+                    return@flow
+                }
+            }
+
+        jwtTokenRepository.getJwtToken()
+            .onSuccess { emit(MainActivityUiState.AfterLogin) }
+            .onError { emit(MainActivityUiState.BeforeLogin) }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, MainActivityUiState.Loading)
+}
+
+sealed interface MainActivityUiState {
+    object Loading : MainActivityUiState
+    object Initial : MainActivityUiState
+    object BeforeLogin : MainActivityUiState
+    object AfterLogin : MainActivityUiState
+}
