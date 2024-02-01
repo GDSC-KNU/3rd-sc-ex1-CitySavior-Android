0f642bd8d2075e2be6a9550f3c5388206eeb3eb4
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-01 14:49:15 +0900

    #8 [Feat]: 마지막 유저 위치정보 저장 repository 구현

--- domain/src/main/kotlin/com/citysavior/android/domain/repository/user/UserRepository.kt
+++ domain/src/main/kotlin/com/citysavior/android/domain/repository/user/UserRepository.kt
@@ -1,8 +1,13 @@
 package com.citysavior.android.domain.repository.user
 
 import com.citysavior.android.domain.model.common.Async
+import com.citysavior.android.domain.model.report.Point
 import com.citysavior.android.domain.model.user.UserInfo
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     suspend fun getUserInfo(): Async<UserInfo>
+
+    suspend fun saveUserPoint(point: Point): Async<Unit>
+    suspend fun getUserPoint(): Flow<Async<Point>>
 }
