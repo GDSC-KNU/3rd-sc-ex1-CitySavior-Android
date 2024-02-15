61dd629e5ff1cbf62087e91c7ef68584c0a69c08
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-15 23:23:33 +0900

    #20 [Feat]: FakeApiClient 400ms 지연시간 추가

--- app/src/main/kotlin/com/citysavior/android/di/FakeApiClient.kt
+++ app/src/main/kotlin/com/citysavior/android/di/FakeApiClient.kt
@@ -17,6 +17,7 @@ import com.citysavior.android.data.dto.user.response.AchieveProgressDto
 import com.citysavior.android.data.dto.user.response.UserInfoResponse
 import com.citysavior.android.domain.model.report.Category
 import com.citysavior.android.domain.model.user.UserRole
+import kotlinx.coroutines.delay
 import okhttp3.MultipartBody
 import retrofit2.Response
 import java.time.LocalDate
@@ -26,22 +27,27 @@ import kotlin.random.Random
 @Singleton
 class FakeApiClient :ApiClient{
     override suspend fun login(request: LoginRequestV1): Response<TokenResponse> {
+        delay(400)
         return Response.success(TokenResponse("token", "refreshToken"))
     }
 
     override suspend fun signup(request: SignupRequestV1): Response<TokenResponse> {
+        delay(400)
         return Response.success(TokenResponse("token", "refreshToken"))
     }
 
     override suspend fun getUserRole(): Response<UserRole> {
+        delay(400)
         return Response.success(UserRole.ADMIN)
     }
 
     override suspend fun changeUserRole(): Response<Unit> {
+        delay(400)
         return Response.success(Unit)
     }
 
     override suspend fun getUserInfo(): Response<UserInfoResponse> {
+        delay(400)
         val response = UserInfoResponse(20,20,30, listOf(
             AchieveProgressDto(
                 Category.AIR_QUALITY,
@@ -56,6 +62,7 @@ class FakeApiClient :ApiClient{
     }
 
     override suspend fun getAchievementInfo(): Response<AchievementInfoResponse> {
+        delay(400)
         return Response.success(AchievementInfoResponse(listOf(
             AchievementDto(
                 "name",
@@ -71,6 +78,7 @@ class FakeApiClient :ApiClient{
         longitude: Double,
         radius: Int
     ): Response<StatisticsByMapResponse> {
+        delay(400)
         val randomR = Random.nextInt(1,100000)
         val details = List(3) {
             val random = Random.nextInt(1,100)
@@ -87,6 +95,7 @@ class FakeApiClient :ApiClient{
         latitude: Double,
         longitude: Double
     ): Response<ReportsByMapResponse> {
+        delay(400)
         val points = List(10) {
             val random1 = Random.nextInt(-100,100)
             val random2 = Random.nextInt(-100,100)
@@ -109,10 +118,12 @@ class FakeApiClient :ApiClient{
         category: Category,
         damageRatio: Int
     ): Response<Long> {
-        return Response.success(Random(0).nextLong())
+        delay(400)
+        return Response.success(Random.nextLong())
     }
 
     override suspend fun getReportDetail(reportId: Long): Response<ReportDetailResponse> {
+        delay(400)
         val imgUrl = if (reportId % 2 == 1L) "https://picsum.photos/200/300" else "https://picsum.photos/200/200"
         return Response.success(ReportDetailResponse(
             "도로가 파여있어요",
@@ -133,10 +144,12 @@ class FakeApiClient :ApiClient{
         reportId: Long,
         request: CreateReportCommentRequest
     ): Response<Long> {
+        delay(400)
         return Response.success(Random.nextLong())
     }
 
     override suspend fun endReport(reportId: Long): Response<Unit> {
+        delay(400)
         return Response.success(Unit)
     }
 }
