5c1de7ce9bedac521c4a128d18ca0f979c5ce1eb
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-21 16:32:51 +0900

    #21 [Feat]: 카테고리 en 추가

--- domain/src/main/kotlin/com/citysavior/android/domain/model/report/Category.kt
+++ domain/src/main/kotlin/com/citysavior/android/domain/model/report/Category.kt
@@ -1,14 +1,20 @@
 package com.citysavior.android.domain.model.report
 
+
+/**
+ * 임시로 카테고리에 ko, en을 Domain Layer에 추가
+ * 향후 Presentation Layer에서 다국어 지원을 위해 변환이 필요함
+ */
 enum class Category(
     val korean : String,
+    val english : String,
 ){
-    ROAD_TRAFFIC("도로 및 교통"),
-    STREET("거리"),
-    BUILD_STRUCTURE("건물 및 구조물"),
-    ENVIRONMENT("환경"),
-    PARK_PUBLIC("공원 및 공공 시설"),
-    CITY_OBSTACLE("도시 장애물"),
-    SCHOOL_ZONE("학교 구역"),
-    OTHER("기타");
+    ROAD_TRAFFIC("도로 및 교통", "Road and Traffic"),
+    STREET("거리", "Street"),
+    BUILD_STRUCTURE("건물 및 구조물", "Building and Structure"),
+    ENVIRONMENT("환경", "Environment"),
+    PARK_PUBLIC("공원 및 공공 시설", "Park and Public Facility"),
+    CITY_OBSTACLE("도시 장애물", "City Obstacle"),
+    SCHOOL_ZONE("학교 구역", "School Zone"),
+    OTHER("기타", "Other");
 }
